474a145f318e56c075d4c60af6f0278878305779
matt <matt@tune.zone>
2015-12-22 22:48:11 +0000

    Day 17

---
+++ 17-1.py
@@ -0,0 +1,52 @@
+from collections import defaultdict
+
+f = open('17-input.py')
+
+def option_exists(options):
+	for pair in options:
+		if sum(pair[0]) < total and sum(pair[1]) > 0:
+			return True
+	return False
+	
+def process_options(options):
+	new_options = []
+	for pair in options:
+		if sum(pair[0]) == total:
+			global count
+			count += 1
+			continue
+		if sum(pair[0]) > total:
+			continue
+		for (i, item) in enumerate(pair[1]):
+			new_left_list = list(pair[0])
+			new_left_list.append(item)
+			new_pair = (new_left_list, pair[1][i+1:])
+			new_options.append(new_pair)
+	return new_options
+
+def main(containers):
+	options = [([], sorted(containers))]
+	while len(options) > 0:
+		options = process_options(options)
+
+# Description of algorithm:
+#
+# Assumptions:
+#   1) Every container has capacity > 0.
+#   2) Containers with the same capacity can be distinguished as 'larger' or 'smaller' (eg. by maintaining an index).
+#
+# Start with a list of pairs of lists, starting with the pair (empty list, all containers).
+# While there are any pairs in the list:
+#
+# For each pair in the list:
+#   1) Pop from the list.
+#   2) If the left list sums to 150, add one to the combination count and continue.
+#   3) If the left list sums to more than 150, continue.
+#   4) Otherwise, for each item in the right list:
+#      - Take a copy of the pair.
+#      - Move the given item from the right list to the left list.
+#      - Add the new pair to the main list.
+count = 0
+total = 150
+main([int(line) for line in f])
+print count
