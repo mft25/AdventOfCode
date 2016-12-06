b7a3dac73d65352a69f35974f5e64a48c8c0c53a
matt <matt@tune.zone>
2016-12-06 13:58:20 +0100

    DAY-06

---
+++ 2016/06-1.py
@@ -0,0 +1,21 @@
+
+f = open('06-input.txt')
+
+def main():
+	letters = []
+	for word in f:
+		word = word.strip()
+		if len(letters) == 0:
+			letters = [{} for letter in word]
+		for (i, letter) in enumerate(word):
+			if letter not in letters[i]:
+				letters[i][letter] = 1
+			else:
+				letters[i][letter] += 1
+	text = ""
+	for dict in letters:
+		list = sorted(dict.items(), key=lambda item: item[1], reverse=True)
+		text += list[0][0]
+	print text	
+
+main()
