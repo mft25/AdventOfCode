a57a1da65edeab8500490854ea202140bcce6558
matt <matt@tune.zone>
2016-12-17 02:06:36 +0100

    DAY-10

---
+++ 2016/10-2.py
@@ -0,0 +1,54 @@
+import re
+
+file = open('10-input.txt')
+
+def add_or_extend(d, key, val):
+	if key not in d:
+		d[key] = [val]
+	else:
+		d[key].append(val)
+
+def read_instructions(file):
+	state = {}
+	state_regex = re.compile("value ([0-9]+) goes to bot ([0-9]+)")
+	actions = {}
+	action_regex = re.compile("bot ([0-9]+) gives low to (bot|output) ([0-9]+) and high to (bot|output) ([0-9]+)")
+	for instruction in file:
+		if state_regex.match(instruction):
+			(chip, bot) = state_regex.match(instruction).groups()
+			add_or_extend(state, int(bot), int(chip))
+		elif action_regex.match(instruction):
+			action = action_regex.match(instruction).groups()
+			actions[int(action[0])] = {
+				"hi": [action[3], int(action[4])],
+				"lo": [action[1], int(action[2])]
+			}
+	return (state, actions)
+
+def find_loaded_bot(state):
+	for bot in state:
+		if len(state[bot]) > 1:
+			return bot
+	
+def main():
+	(state, actions) = read_instructions(file)
+	output = {}
+	done = False
+	while True:
+		bot = find_loaded_bot(state)
+		if bot is None:
+			break
+		lo_action = actions[bot]["lo"]
+		if lo_action[0] == "bot":
+			add_or_extend(state, lo_action[1], min(state[bot]))
+		else:
+			add_or_extend(output, lo_action[1], min(state[bot]))
+		hi_action = actions[bot]["hi"]
+		if hi_action[0] == "bot":
+			add_or_extend(state, hi_action[1], max(state[bot]))
+		else:
+			add_or_extend(output, hi_action[1], max(state[bot]))
+		state[bot] = []
+	print output[0][0] * output[1][0] * output[2][0]
+	
+main()
